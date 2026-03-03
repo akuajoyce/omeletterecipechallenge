9f0c6ebfcd48dd06bd5f905f89703e809e9b8927
Your Name <ashawaabbey@gmail.com>
2026-03-03 14:32:46 +0000

    worked on fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import '@fontsource/young-serif/400.css';
+import '@fontsource/young-serif/400.css'
+import '@fontsource/outfit/400.css'
+import '@fontsource/outfit/600.css'
+import '@fontsource/outfit/700.css'
 
 
 createRoot(document.getElementById('root')).render(
